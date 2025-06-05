cc96c9baf2acbf5d95f3d50d5220315a618a04d6
nicky-ops <nickyrutto@gmail.com>
2025-06-06 00:01:44 +0300

    fix(url): update bookmarklet source base url

--- app/images/templates/bookmarklet_launcher.js
+++ app/images/templates/bookmarklet_launcher.js
@@ -1,7 +1,7 @@
 (function(){
     if(!window.bookmarklet) {
         bookmarklet_js = document.body.appendChild(document.createElement('script'));
-        bookmarklet_js.src = '//social-bookmarking-website.onrender.com/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
+        bookmarklet_js.src = '//pixmark.kiprutonick.work/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
         window.bookmarklet = true;
     }
     else {
