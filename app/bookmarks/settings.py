e733e71b8160ee63109148aa1a17d3278aca534a
nicky-ops <nickyrutto@gmail.com>
2025-06-05 20:53:19 +0300

    feat(db): migrate to prod database

--- app/bookmarks/settings.py
+++ app/bookmarks/settings.py
@@ -105,8 +105,14 @@ WSGI_APPLICATION = "bookmarks.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.{}'.format(
+            os.getenv('DATABASE_ENGINE', 'sqlite3')
+        ),
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USERNAME'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
